8a3e6f955bf6687d928311dc1b025330159ab2f9
Leah Rothschild <leahisrothschild@gmail.com>
2026-01-10 19:47:26 +0100

    updating main.py and removing wake.yml locally

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ STREAMLIT_URL = "https://leah-rothschild.streamlit.app/"
 
 def main():
     options = Options()
-    options.add_argument(--headless=new)
-    options.add_argument(--no-sandbox)
-    options.add_argument(--disable-dev-shm-usage)
-    options.add_argument(--disable-gpu)
-    options.add_argument(--window-size=1920,1080)
+    options.add_argument("headless=new")
+    options.add_argument("no-sandbox")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("disable-gpu")
+    options.add_argument("window-size=1920,1080")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
